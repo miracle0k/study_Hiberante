ab66f463b785eaeee04169ad955e6872d3d0d82b
Miracle <kmiracle@gmail.com>
2010-10-15 10:17:16 +0900

    ch04.2.영속 컬렉션

--- src/com/oreilly/hh/CreateTest.java
+++ src/com/oreilly/hh/CreateTest.java
@@ -7,18 +7,37 @@ import com.oreilly.hh.data.*;
 
 import java.sql.Time;
 import java.util.Date;
+import java.util.HashSet;
 
 /**
  * Create sample data, letting Hibernate persist it for us.
  */
 public class CreateTest {
 
+	public static Artist getArtist(String name, boolean create, Session session) {
+		Query query = session.getNamedQuery("com.oreilly.hh.artistByName");
+		query.setString("name", name);
+		
+		Artist found = (Artist) query.uniqueResult();
+		
+		if(found == null && create) {
+			found = new Artist(name, new HashSet<Track>());
+			session.save(found);
+		}
+		
+		return found;
+	}
+	
+	private static void addTrackArtist(Track track, Artist artist) {
+		track.getArtists().add(artist);
+	}
+	
     public static void main(String args[]) throws Exception {
-        // Create a configuration based on the XML file we've put
+    	  // Create a configuration based on the XML file we've put
         // in the standard place.
         Configuration config = new Configuration();
         config.configure();
-        
+
         // Get the session factory we can use for persistence
         SessionFactory sessionFactory = config.buildSessionFactory();
 
@@ -26,40 +45,75 @@ public class CreateTest {
         Session session = sessionFactory.openSession();
         Transaction tx = null;
         try {
-            // Create some data and persist it
-            tx = session.beginTransaction();
+          // Create some data and persist it
+          tx = session.beginTransaction();
 
-            Track track = new Track("Russian Trance",
-                                    "vol2/album610/track02.mp3",
-                                    Time.valueOf("00:03:30"), new Date(),
-                                    (short)0);
-            session.save(track);
+          Track track = new Track("Russian Trance",
+                                  "vol2/album610/track02.mp3",
+                                  Time.valueOf("00:03:30"),
+                                  new HashSet<Artist>(),
+                                  new Date(), (short)0);
+          addTrackArtist(track, getArtist("PPK", true, session));
+          session.save(track);
 
-            track = new Track("Video Killed the Radio Star",
-                              "vol2/album611/track12.mp3",
-                              Time.valueOf("00:03:49"), new Date(),
-                              (short)0);
-            session.save(track);
+          track = new Track("Video Killed the Radio Star",
+                            "vol2/album611/track12.mp3",
+                            Time.valueOf("00:03:49"), new HashSet<Artist>(),
+                            new Date(), (short)0);
+          addTrackArtist(track, getArtist("The Buggles", true, session));
+          session.save(track);
+                
+          track = new Track("Gravity's Angel",
+                            "vol2/album175/track03.mp3",
+                            Time.valueOf("00:06:06"), new HashSet<Artist>(),
+                            new Date(), (short)0);
+          addTrackArtist(track, getArtist("Laurie Anderson", true, session));
+          session.save(track);
 
-            
-             track = new Track("Gravity's Angel",
-                              "vol2/album175/track03.mp3",
-                               Time.valueOf("00:06:06"), new Date(),
-                               (short)0);
-            session.save(track);
+          track = new Track("Adagio for Strings (Ferry Corsten Remix)",
+                            "vol2/album972/track01.mp3",
+                            Time.valueOf("00:06:35"), new HashSet<Artist>(),
+                            new Date(), (short)0);
+          addTrackArtist(track, getArtist("William Orbit", true, session));
+          addTrackArtist(track, getArtist("Ferry Corsten", true, session));
+          addTrackArtist(track, getArtist("Samuel Barber", true, session));
+          session.save(track);
 
-            // We're done; make our changes permanent
-            tx.commit();
+          track = new Track("Adagio for Strings (ATB Remix)",
+                            "vol2/album972/track02.mp3",
+                            Time.valueOf("00:07:39"), new HashSet<Artist>(),
+                            new Date(), (short)0);
+          addTrackArtist(track, getArtist("William Orbit", true, session));
+          addTrackArtist(track, getArtist("ATB", true, session));
+          addTrackArtist(track, getArtist("Samuel Barber", true, session));
+          session.save(track);
+
+          track = new Track("The World '99",
+                            "vol2/singles/pvw99.mp3",
+                            Time.valueOf("00:07:05"), new HashSet<Artist>(),
+                            new Date(), (short)0);
+          addTrackArtist(track, getArtist("Pulp Victim", true, session));
+          addTrackArtist(track, getArtist("Ferry Corsten", true, session));
+          session.save(track);
+
+          track = new Track("Test Tone 1",
+                            "vol2/singles/test01.mp3",
+                            Time.valueOf("00:00:10"), new HashSet<Artist>(),
+                            new Date(), (short)0);
+          session.save(track);
+
+          // We're done; make our changes permanent
+          tx.commit();
 
         } catch (Exception e) {
-            if (tx != null) {
-                // Something went wrong; discard all partial changes
-                tx.rollback();
-            }
-            throw new Exception("Transaction failed", e);
+          if (tx != null) {
+            // Something went wrong; discard all partial changes
+            tx.rollback();
+          }
+          throw new Exception("Transaction failed", e);
         } finally {
-            // No matter what, close the session
-            session.close();
+          // No matter what, close the session
+         session.close();
         }
 
         // Clean up after ourselves
